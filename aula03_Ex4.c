18dc479642fa7f7c55b36709827fe51c3b6d8b6f
João Vitor de Oliveira <88735946+JoaoVitor2003@users.noreply.github.com>
2022-10-21 15:56:47 -0300

    Add files via upload

---
+++ aula03_Ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// mostrar todos os numeros entre 0 e 10
+int main()
+{
+    int contador=0;
+    do
+    {
+       printf("%d \n", contador);      
+       contador++;  
+    } while (contador <= 10);
+  system("PAUSE");    
+  return 0;
+}
